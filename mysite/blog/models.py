6b5e363f001f3fc93176ac8d4c29e3076f413cac
ahmed sayed <ahmed.sayed.ams@gmail.com>
2023-10-24 00:36:24 +0300

    first commit

---
+++ mysite/blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+# Create your models here.
+
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'draft'
+        PUBLISHED = 'published'
+
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')
+    body = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=Status.choices, default=Status.DRAFT)
+
+    class Meta:
+        ordering = ['-published' ]   
+        indexes = [
+            models.Index(fields=['-published']),
+        ]
+
+    def __str__ (self):
+        return self.title
+
+def get_absolute_url(self):
+    return reverse('blog:post_detail', args=[self.id])
