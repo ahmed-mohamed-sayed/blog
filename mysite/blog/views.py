5dbc00b0ec13c5bd3aa5160da9cfa6c712719235
ahmed sayed <ahmed.sayed.ams@gmail.com>
2023-10-29 02:11:36 +0300

    class based views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,15 +1,24 @@
+from django.views.generic.list import ListView
 from django.shortcuts import get_object_or_404, render
 from.models import Post
 from django.http import Http404
 from django.core.paginator import Paginator , EmptyPage, PageNotAnInteger
 
-# Create your views here.
-def post_list(request):
-    post_list = Post.objects.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page',1)
-    posts = paginator.get_page(page_number)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    # Create your views here.
+    # def post_list(request):
+    #     post_list = Post.objects.all()
+    #     paginator = Paginator(post_list, 3)
+    #     page_number = request.GET.get('page',1)
+    #     posts = paginator.get_page(page_number)
+    #     return render(request, 'blog/post/list.html', {'posts': posts})
+
+class PostListView(ListView):
+    ''' alternative way to create a list view '''
+    model = Post
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
+        
 
 # Create your views here.
 def post_detail(request, year, month, day, slug):
