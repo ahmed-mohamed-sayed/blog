d0eb6b45a36acf04297d219653358f3e21c9ca9b
ahmed sayed <ahmed.sayed.ams@gmail.com>
2023-10-29 00:32:26 +0300

    seo friendly urls

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
         PUBLISHED = 'published'
 
     title = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
+    slug = models.SlugField(max_length=100 , unique_for_date='published')
     author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')
     body = models.TextField()
     published = models.DateTimeField(default=timezone.now)
@@ -29,5 +29,5 @@ class Post(models.Model):
     def __str__ (self):
         return self.title
 
-def get_absolute_url(self):
-    return reverse('blog:post_detail', args=[self.id])
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.published.year, self.published.month, self.published.day, self.slug])
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from.models import Post
 from django.http import Http404
 
@@ -7,11 +7,6 @@ def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
 
-def post_detail(request, id):
-    try:
-        post = Post.objects.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("Post does not exist")
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, published__year=year, published__month=month, published__day=day, slug=slug)
     return render(request, 'blog/post/detail.html', {'post': post})
-
-# D:\blog\src\mysite\blog\templates\posts\list.html
